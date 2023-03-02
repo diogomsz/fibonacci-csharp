c9e1cc268601660d96fb5dd48a6c23a1866ea770
Diogo Martins de Assis <diogomartinsdeassis1@gmail.com>
2023-03-02 19:45:00 -0300

    Adicionar arquivos de projeto.

---
+++ Fibonacci/FibonacciClass.cs
@@ -0,0 +1,30 @@
+﻿
+
+
+public class FibonacciClass
+{
+    public static List<int> Fibo(int n)
+    {
+        List<int> valores = new();
+
+        int x = 1, y = 0, z = 0;
+        valores.Add(0);
+        while (n >= z)
+        {
+            z = x + y;
+            valores.Add(z);
+            y = x;
+            x = z;
+        }
+
+        return valores;
+    }
+
+    public static void Imprimir(List<int> valores)
+    {
+        valores.ForEach(x =>
+        {
+            Console.WriteLine(x);
+        });
+    }
+}
---
+++ Fibonacci/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Digite um termo para sequência: ");
+        int n = int.Parse(Console.ReadLine());
+
+        List<int> v = FibonacciClass.Fibo(n);
+        bool encontrou = false;
+
+        v.ForEach(x =>
+        {
+            if (x == n)
+            {
+                encontrou = true;
+            }
+        });
+
+        if (encontrou)
+        {
+            FibonacciClass.Imprimir(v);
+            Console.WriteLine("Pertence à sequência");
+        }
+        else
+        {
+            FibonacciClass.Imprimir(v);
+            Console.WriteLine("Não pertence à sequência");
+        }
+    }
+}
